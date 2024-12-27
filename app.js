a3c4f929a07df95aa972bb9a168a44d6f6ff3731
slvDev <slava.serbov@gmail.com>
2024-12-27 11:56:31 +0200

    move to youtube-dl-exec

--- app.js
+++ app.js
@@ -1,40 +1,67 @@
 const fs = require('fs');
 const path = require('path');
-const youtubedl = require('youtube-dl');
+const youtubedl = require('youtube-dl-exec');
 
-(async () => {    
-    const url = process.argv[2]
-    // Get Video Info
-    let videoInfo = await new Promise((resolve, reject) => {
-        youtubedl.getInfo(url, (err, info) => {
-            if (err) reject(err)
-            let videoInfo = {
-                title: info.title,
-                description: info.description,
-            }
-            resolve(videoInfo)
-        })
-    })
-    let filePath = path.join(__dirname, 'videos', videoInfo.title.replace(/\|/g, ''))
-    if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true })
+(async () => {
+    try {
+        const url = process.argv[2];
+        if (!url) throw new Error('Please provide a YouTube URL');
 
-    fs.writeFileSync(`${filePath}/info.txt`, 'Title\n' + videoInfo.title + '\n\nDescription\n' + videoInfo.description)
-    console.log(`Info file created`)
-    
-    // Download Thumbnail
-    await new Promise((resolve, reject) => {
-        youtubedl.getThumbs(url, { all: false, cwd: filePath }, (err, files) => {
-          if (err) console.log(err)
-          console.log(`Thumbnail file downloaded`)
-          resolve()
-        })
-    })
+        // Get Video Info
+        console.log('Fetching video info...');
+        const videoInfo = await youtubedl(url, {
+            dumpSingleJson: true,
+            noCheckCertificates: true,
+            noWarnings: true,
+            preferFreeFormats: true,
+            addHeader: [
+                'referer:youtube.com',
+                'user-agent:googlebot'
+            ]
+        });
 
-    // Download Video
-    console.log('Download started')
-    youtubedl.exec(url,['-o', `${filePath}/%(title)s.%(ext)s`], {}, (err, output) => {
-        if(err) console.log(err)
-        console.log(output.join('\n'))
-    })
+        // Create directory
+        const filePath = path.join(__dirname, 'videos', videoInfo.title.replace(/[/\\?%*:|"<>]/g, '-'));
+        if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true });
 
+        // Save info file
+        fs.writeFileSync(
+            `${filePath}/info.txt`,
+            'Title\n' + videoInfo.title + '\n\nDescription\n' + videoInfo.description
+        );
+        console.log(`Info file created`);
+
+        // Download Thumbnail
+        console.log('Downloading thumbnail...');
+        await youtubedl(url, {
+            skipDownload: true,
+            writeThumbnail: true,
+            output: `${filePath}/thumbnail.jpg`,
+            noCheckCertificates: true,
+            noWarnings: true,
+            addHeader: [
+                'referer:youtube.com',
+                'user-agent:googlebot'
+            ]
+        });
+        console.log(`Thumbnail downloaded`);
+
+        // Download Video
+        console.log('Downloading video...');
+        await youtubedl(url, {
+            output: `${filePath}/%(title)s.%(ext)s`,
+            format: 'best',
+            noCheckCertificates: true,
+            noWarnings: true,
+            preferFreeFormats: true,
+            addHeader: [
+                'referer:youtube.com',
+                'user-agent:googlebot'
+            ]
+        });
+        console.log('Download completed');
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 })();
